cd3b6485c56b1a2cd4a2be684a6cd79cb524e1ac
SPARTANX21 <panushah57@gmail.com>
2025-02-27 08:07:30 +0530

    first script - create DWH

---
+++ Scripts/Script1.sql
@@ -0,0 +1,16 @@
+-- Create Database "SQL_Datawarehouse"
+
+USE master;
+
+CREATE DATABASE DataWarehouse;
+
+USE DataWarehouse;
+
+GO
+CREATE SCHEMA Bronze;
+GO
+CREATE SCHEMA Silver;
+go
+CREATE SCHEMA Gold;
+go
+
